0e4f0074132e9e5b27c8f96fe0f55ed04a418d52
decapix <decapixd@gmail.com>
2025-04-05 18:26:23 +0200

    react init

--- web3auth-front/src/App.js
+++ web3auth-front/src/App.js
@@ -1,14 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { Web3Auth } from "@web3auth/modal";
-
-const web3auth = new Web3Auth({
-  clientId: "YOUR_WEB3AUTH_CLIENT_ID",
-  chainConfig: {
-    chainNamespace: "eip155",
-    chainId: "0x1", // Ethereum mainnet
-  },
-});
 
 function App() {
   return (
@@ -32,8 +23,3 @@ function App() {
 }
 
 export default App;
-await web3auth.initModal();
-const provider = await web3auth.connect();
-const web3 = new Web3(provider);
-const accounts = await web3.eth.getAccounts();
-console.log(accounts); // Affiche les comptes de l'utilisateur
