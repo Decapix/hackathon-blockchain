ffc54ed4df8b61c7487ed731d36306dba416a2a1
Dimmis5 <zhangdimeo@gmail.com>
2025-04-05 21:23:04 +0200

    add ethers.js

---
+++ SignWithEthreum/ethers.js
@@ -0,0 +1,4 @@
+const { ethers } = require("ethers");
+
+// Connecte-toi à Bahamut via un provider
+const provider = new ethers.JsonRpcProvider("https://bahamut.infura.io/v3/YOUR_INFURA_PROJECT_ID"); // Remplace avec ton URL provider Bahamut
