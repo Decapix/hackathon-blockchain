9080ec46b5efaf764e476fb8a5a114c7a9247cdd
decapix <decapixd@gmail.com>
2025-04-05 17:59:55 +0200

    remove useless feature

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Query
 import logging
 from utils_session import *
-from web3dev import get_test_score, contract_address, infura_url, record_consent, start_test, complete_test
 from __init__ import supabase
-from web3 import Web3
 from models import UserModel
 from db import db, User
 
@@ -18,57 +16,8 @@ async def home(n: int = 10):
 
 
 
-# Exemple d'utilisation
-if __name__ == "__main__":
-    contract_abi = [
-        # Ajoutez ici l'ABI de votre contrat
-    ]
-    user_address = '0xAdresseDeLUtilisateur'
-
-    try:
-        score = get_test_score(infura_url, contract_address, contract_abi, user_address)
-        print(f"Le score de l'utilisateur est : {score}")
-    except Exception as e:
-        print(e)
 
 
-
-@router.get("/items/")
-async def read_item(name: str = Query(..., description="The name of the item"),
-                    number: int = Query(..., description="The number associated with the item")):
-    logging.error("read_item call")
-    #
-    user_address = '0xAdresseDeLUtilisateur'
-    score = get_test_score(infura_url, contract_address, contract_abi, user_address)
-    return {"name": name, "number": number}
-
-# Modèle de données pour la requête
-class TestRequest(BaseModel):
-    email: str
-    test_id: str
-    total_questions: int
-    correct_answers: int
-    fraud_score: int
-    metadata_uri: str
-
-@router.post("/make_test")
-async def make_test(request: TestRequest):
-    try:
-        email_hash = Web3.keccak(text=request.email)
-        test_id = Web3.keccak(text=request.test_id)
-
-        # Enregistrer le consentement
-        record_consent(email_hash, test_id)
-
-        # Démarrer le test
-        start_test(test_id, request.total_questions)
-
-        # Compléter le test
-        complete_test(test_id, request.correct_answers, request.fraud_score, request.metadata_uri)
-
-        return {"status": "Test completed successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/create_user/")
