d9dd54321f58e83c2a652cae258785118e357b2f
Alan Tuning <136696905+Degnel@users.noreply.github.com>
2025-04-06 12:04:16 +0200

    New questions

--- frontend/questions.py
+++ frontend/questions.py
@@ -1,12 +1,36 @@
 QUESTIONS = {
-    "exam1": [
-        {"question": "Question 1", "options": ["A", "B", "C", "D"], "correct_answer": "A"},
-        {"question": "Question 2", "options": ["A", "B", "C", "D"], "correct_answer": "B"},
-        {"question": "Question 3", "options": ["A", "B", "C", "D"], "correct_answer": "C"},
-        {"question": "Question 4", "options": ["A", "B", "C", "D"], "correct_answer": "D"},
-        {"question": "Question 5", "options": ["A", "B", "C", "D"], "correct_answer": "A"},
+    "Blockchain Pro": [
+        {
+            "question": "What is a blockchain?",
+            "options": [
+                "A. A type of digital spreadsheet shared across a network",
+                "B. A chain used to lock digital files",
+                "C. A type of antivirus software"
+            ],
+            "correct_answer": "A"
+        },
+        {
+            "question": "Which of the following is a key feature of blockchain technology?",
+            "options": [
+                "A. Centralized control",
+                "B. Transparency and immutability",
+                "C. Unlimited editing rights for users",
+                "D. Ability to hide all transaction history"
+            ],
+            "correct_answer": "B"
+        },
+        {
+            "question": "What happens on the blockchain when users mine cryptocurrency?",
+            "options": [
+                "A. Transactions are verified and recorded in a decentralized ledger to maintain network security.",
+                "B. Crypto mining is just a way to dig a financial hole...",
+                "C. Users earn loyalty points from retailers",
+                "D. Mining decreases the value of the currency"
+            ],
+            "correct_answer": "A"
+        }
     ],
-    "exam2": [
+    "PhD (Permanent head Damage)": [
         {"question": "Question 1", "options": ["A", "B", "C", "D"], "correct_answer": "A"},
         {"question": "Question 2", "options": ["A", "B", "C", "D"], "correct_answer": "B"},
         {"question": "Question 3", "options": ["A", "B", "C", "D"], "correct_answer": "C"},
